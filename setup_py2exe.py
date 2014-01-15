27e127e725fe3d891b973709bb3dccd64c47e313
Cynthia Kurtz <cfkurtz@kurtz-fernhout.com>
2014-01-15 20:15:09 +0000

    flailing while trying to create a windows exe

--- setup_py2exe.py
+++ setup_py2exe.py
@@ -5,6 +5,20 @@ Usage:
 
 from distutils.core import setup
 import py2exe
+import numpy
 
-setup(console=['NarraCat.py'])
+from distutils.filelist import findall
+import os
+import matplotlib
+matplotlibdatadir = matplotlib.get_data_path()
+matplotlibdata = findall(matplotlibdatadir)
+matplotlibdata_files = []
+for f in matplotlibdata:
+    dirname = os.path.join('matplotlibdata', f[len(matplotlibdatadir)+1:])
+    matplotlibdata_files.append((os.path.split(dirname)[0], [f]))
+
+setup(console=['NarraCat.py'],
+      options={'py2exe': {'packages':['matplotlib'],}},
+      data_files=matplotlibdata_files
+      )
 
