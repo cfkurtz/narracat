ca5696e9cf539b47c7b6f12591e1ed25957fdc85
Cynthia Kurtz <cfkurtz@kurtz-fernhout.com>
2014-01-15 19:34:41 +0000

    was trying to make it so you could choose the config file directory up front, instead of using a command-line argument; but it didn't work. had to make a Tk root object to put up the file chooser dialog, and that breaks the main window later on. why? i don't know. not fixable. went back to the command line argument for now.

--- narracat_constants.py
+++ narracat_constants.py
@@ -11,8 +11,11 @@
 # -----------------------------------------------------------------------------------------------------------------
 
 import os, sys, matplotlib
+from narracat_utils import stringUpTo
 
-# specify base path in command-line argument, or leave blank to use path where script is located
+sys.stdout = sys.stderr
+
+# specify base path in command-line argument, or choose from pop-up directory chooser
 
 # narracat expects to find files in these folders:
 #	base
@@ -30,12 +33,39 @@ if len(sys.argv) > 1:
 	BASE_PATH = sys.argv[1]
 else:
 	BASE_PATH = os.path.dirname(sys.argv[0])
+
+defaultConfigFileName = CONFIG_PATH = BASE_PATH + os.sep + "config" + os.sep + "narracat_config.py"
+
+configFileName = defaultConfigFileName
+
+# was trying to use this to get config file by chooser
+# but it doesn't work, it messes up the focus on the launcher window later
+# so i must stay (for now) with a command line argument as before
+"""
+from Tkinter import Toplevel
+from tkFileDialog import askopenfilename
+
+#top = Toplevel()
+#top.withdraw() # make invisible
+configFileName = askopenfilename(initialfile=defaultConfigFileName, 
+								parent=None,\
+						defaultextension="py",\
+						title="Choose Your NarraCat Configuration File (See Readme.txt)")
+						#, mustexist=True)
+#top.destroy()
+"""
+
+basePathWithConfig = os.path.dirname(configFileName)
+BASE_PATH = stringUpTo(basePathWithConfig, os.sep + "config")
+
 print 'Starting narracat with Base Path"', BASE_PATH, '"'
 print 'matplotlib version', matplotlib.__version__
 
 DATA_PATH = BASE_PATH + os.sep + "data" + os.sep
 if not os.path.exists(DATA_PATH):
 	os.mkdir(DATA_PATH)
+	
+SHOW_TESTING_BUTTONS = False
 
 # default file names - can override in local narracat_config file
 PICKLE_FILE_NAME = "pickle.txt"
@@ -64,3 +94,4 @@ else:
 	OUTPUT_PATH = BASE_PATH + os.sep + "output" + os.sep
 if not os.path.exists(OUTPUT_PATH):
 	os.mkdir(OUTPUT_PATH)
+	
